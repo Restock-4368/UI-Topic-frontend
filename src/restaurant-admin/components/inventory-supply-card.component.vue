2099c0bcf1bf96da266475af5d7900ba7e208a15
JulioXC4 <danielc4nt@gmail.com>
2025-05-15 02:21:57 -0500

    fix: fix add inventory (components)

---
+++ src/restaurant-admin/components/inventory-supply-card.component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="border-1 surface-border border-round p-3 m-2 text-center">
+    <h4>{{ supply.name }}</h4>
+    <p class="text-sm">{{ supply.description }}</p>
+    <p><strong>{{ supply.category }}</strong> - {{ supply.unit }}</p>
+
+    <pv-button
+        icon="pi pi-pencil"
+        label="Editar"
+        size="small"
+        class="mt-2"
+        @click="$emit('edit', supply)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InventorySupplyCard',
+  props: {
+    supply: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
