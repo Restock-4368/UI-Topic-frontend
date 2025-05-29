c692a426c32fced5aecf33d8320a899e4971ef5d
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-05-29 07:14:48 -0500

    refactor(supplier-orders-supplies): change supplier orders attribute.

--- src/restock/resource/orders-to-suppliers/model/order-to-supplier-supply.entity.js
+++ src/restock/resource/orders-to-suppliers/model/order-to-supplier-supply.entity.js
@@ -1,8 +1,8 @@
 export class OrderToSupplierSupply {
-    constructor({ order_to_supplier_id, supply_id, stock, accepted }) {
+    constructor({ order_to_supplier_id, supply_id, quantity, accepted }) {
         this.orderId = order_to_supplier_id;
         this.supplyId = supply_id;
-        this.stock = stock;
+        this.quantity = quantity;
         this.accepted = accepted;
     }
 }
