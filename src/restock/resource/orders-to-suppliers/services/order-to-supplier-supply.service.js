7b0080c7e9fc0b60ddabf943a240ed3c0a3d50e9
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-05-29 18:20:41 -0500

    refactor(orders-services): delete redundant methods of orders services.

--- src/restock/resource/orders-to-suppliers/services/order-to-supplier-supply.service.js
+++ src/restock/resource/orders-to-suppliers/services/order-to-supplier-supply.service.js
@@ -8,14 +8,6 @@ export class OrderToSupplierSupplyService extends BaseService {
         return this.getByQuery("order_to_supplier_id", orderId);
     }
 
-    updateAcceptance(orderId, supplyId, accepted) {
-        return httpInstance.patch(`${this.resourceEndpoint}`, {
-            order_to_supplier_id: orderId,
-            supply_id: supplyId,
-            accepted
-        });
-    }
-
     getBySupplyId(supplyId) {
         return this.getByQuery("supply_id", supplyId);
     }
--- src/restock/resource/orders-to-suppliers/services/order-to-supplier.service.js
+++ src/restock/resource/orders-to-suppliers/services/order-to-supplier.service.js
@@ -2,17 +2,13 @@ import { BaseService } from "src/shared/services/base.service.js";
 
 export class OrderToSupplierService extends BaseService {
     resourceEndpoint = import.meta.env.VITE_ORDERS_TO_SUPPLIER_ENDPOINT_PATH;
-    
+
     getBySupplierId(supplierId) {
         return this.getByQuery("supplierId", supplierId);
     }
 
     getByState(stateId) {
         return this.getByQuery("stateId", stateId);
-    }
-
-    acceptOrder(orderId) {
-        return httpInstance.patch(`${this.resourceEndpoint}/${orderId}/accept`);
     }
 
     getByRestaurantId(supplierId) {
