66c951dc1424eeb0460e24da766b690ae07c5292
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-05-29 23:14:58 -0500

    refactor(supply): delete getAll method of supply service.

--- src/restock/resource/inventory/services/supply.service.js
+++ src/restock/resource/inventory/services/supply.service.js
@@ -6,27 +6,5 @@ export class SupplyService extends BaseService {
 
     resourceEndpoint = import.meta.env.VITE_SUPPLIES_ENDPOINT_PATH;
 
-    async getAll() {
-        const response = await super.getAll();
-        return response.data.map(s => new Supply(s));
-    }
 
-    async getById(id) {
-        const response = await super.getById(id);
-        return new Supply(response.data);
-    }
-
-    async create(supplyData) {
-        const response = await super.create(supplyData);
-        return new Supply(response.data);
-    }
-
-    async update(id, supplyData) {
-        const response = await super.update(id, supplyData);
-        return new Supply(response.data);
-    }
-
-    async delete(id) {
-        return await super.delete(id);
-    }
 }
