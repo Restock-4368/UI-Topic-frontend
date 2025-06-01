b99c2ab9be6747f4929be92082930603b6dc9e13
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-01 15:59:09 -0500

    fix(unit-measurement): fix unit measurement endpoint call in the service.

--- src/restock/resource/inventory/services/unit-measurement.service.js
+++ src/restock/resource/inventory/services/unit-measurement.service.js
@@ -3,11 +3,11 @@ import {UnitMeasurement} from "../model/unit-measurement.entity.js";
 
 export class UnitMeasurementService extends BaseService {
     constructor() {
-        super(import.meta.env.VITE_UNIT_MEASUREMENTS_ENDPOINT_PATH); // e.g. '/unit_measurements'
+        super(import.meta.env.VITE_UNITS_MEASUREMENT_ENDPOINT_PATH); // e.g. '/unit_measurements'
     }
 
-    async getAll() {
-        const response = await super.getAll();
-        return response.data.map(u => new UnitMeasurement(u));
-    }
+    // async getAll() {
+    //     const response = await super.getAll();
+    //     return response.data.map(u => new UnitMeasurement(u));
+    // }
 }
