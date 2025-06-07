34007dd89191020a95e3815fae2f13f92105b682
jahazielgg <u202319831@upc.edu.pe>
2025-06-07 12:58:22 -0500

    feat(recipes): refactor RecipeService and RecipeSupplyService to simplify data handling

--- src/restock/planning/services/recipe-supply.service.js
+++ src/restock/planning/services/recipe-supply.service.js
@@ -1,13 +1,11 @@
 import {BaseService} from "../../../shared/services/base.service.js";
-import {RecipeSupply} from "../model/recipe-supply.entity.js";
 
 export class RecipeSupplyService extends BaseService {
 
     resourceEndpoint = import.meta.env.VITE_RECIPES_SUPPLIES_ENDPOINT_PATH;
 
-    async getByRecipe(recipeId) {
-        const response = await this.getByQuery('recipe_id', recipeId);
-        return response.data.map(rs => new RecipeSupply(rs));
+    getByRecipe(recipeId) {
+        return this.getByQuery('recipe_id', recipeId);
     }
 
 
@@ -23,7 +21,8 @@ export class RecipeSupplyService extends BaseService {
     }
 
     async deleteByRecipe(recipeId) {
-        const recipeSupplies = await this.getByRecipe(recipeId);
+        const response = await this.getByRecipe(recipeId);
+        const recipeSupplies = response.data;
         const deletions = recipeSupplies.map(rs => this.delete(rs.id));
         return Promise.all(deletions);
     }
--- src/restock/planning/services/recipe.service.js
+++ src/restock/planning/services/recipe.service.js
@@ -1,31 +1,16 @@
 import {BaseService} from "../../../shared/services/base.service.js";
-import {Recipe} from "../model/recipe.entity.js";
 
 export class RecipeService extends BaseService {
 
     resourceEndpoint = import.meta.env.VITE_RECIPES_ENDPOINT_PATH;
 
-    async getAll() {
-        const response = await super.getAll();
-        return response.data.map(r => new Recipe(r));
+    create(recipeData) {
+        const {supplies, ...payload} = recipeData || {};
+        return super.create(payload);
     }
 
-    async getById(id) {
-        const response = await super.getById(id);
-        return new Recipe(response.data);
-    }
-
-    async create(recipeData) {
-        const response = await super.create(recipeData);
-        return new Recipe(response.data);
-    }
-
-    async update(id, recipeData) {
-        const response = await super.update(id, recipeData);
-        return new Recipe(response.data);
-    }
-
-    async delete(id) {
-        return super.delete(id);
+    update(id, recipeData) {
+        const {supplies, ...payload} = recipeData || {};
+        return super.update(id, payload);
     }
 }
