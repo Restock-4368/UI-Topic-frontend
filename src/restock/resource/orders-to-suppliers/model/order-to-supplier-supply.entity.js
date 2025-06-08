3008a2b8aa6fdd153a6485f39e5ba234250c08b6
GabrielaShapiama <137426299+GabrielaShapiama28@users.noreply.github.com>
2025-06-07 21:47:01 -0500

    feat(orders): add attribute id to orders supplies.

--- src/restock/resource/orders-to-suppliers/model/order-to-supplier-supply.entity.js
+++ src/restock/resource/orders-to-suppliers/model/order-to-supplier-supply.entity.js
@@ -1,5 +1,6 @@
 export class OrderToSupplierSupply {
-    constructor({ order_to_supplier_id = 0, supply_id = 0, quantity = '', accepted = '' }) {
+    constructor({ id = 0, order_to_supplier_id = 0, supply_id = 0, quantity = '', accepted = '' }) {
+        this.id = id;
         this.orderId = order_to_supplier_id;
         this.supplyId = supply_id;
         this.quantity = quantity;
