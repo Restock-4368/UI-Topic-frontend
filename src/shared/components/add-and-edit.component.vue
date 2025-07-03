7f0392574c592a3cde50eaf438311512c2ee9621
jahazielgg <u202319831@upc.edu.pe>
2025-07-03 13:50:22 -0500

    feat(recipes): update recipes overview to include supply details and enhance loading logic

--- src/shared/components/add-and-edit.component.vue
+++ src/shared/components/add-and-edit.component.vue
@@ -18,10 +18,6 @@ export default {
   methods: {
     handleUpload(event, fieldName) {
       const file = event.files[0];
-      console.log('📁 Upload triggered:', event.files);
-      console.log('🧪 Sending to Cloudinary:');
-      console.log('Upload preset:', 'uitopic');
-      console.log('File:', file.name);
 
       const formData = new FormData();
       formData.append('file', file);
